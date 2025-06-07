214975f798c09637a0f6a604ad9b1b41556b97c0
Sahil <sahilsharma2000dtc@gmail.com>
2025-06-07 22:29:33 +0530

    suite/describe in unit testing

--- test/app.test.ts
+++ test/app.test.ts
@@ -6,13 +6,29 @@ A -> Arrange
 A -> Act
 A -> Assert
 */
-import { test} from 'bun:test'; // in node env we have import as "node:test" here i'm using bun js runtime environment.
+import {test, describe} from 'bun:test'; // in node env we have import as "node:test" here i'm using bun js runtime environment.
 import assert from 'node:assert';
-import { greet } from '../app';
+import { greet, greetInJapanease } from '../app';
+
+
 
 test('greet returns the correct greeting', () => {
     //AAA
     const expected = "Hello, World!"
     const actual = greet("World");
     assert.strictEqual(actual, expected);
-})
+});
+
+// putting one or more test in club which referred as suites -> In Bun we are using describe and in node we will be using suite
+describe("Greeting functions", () => {
+    test("greet returns the correct greeting in english", () => {
+        const expected = "Hello, World!";
+        const actual = greet("World");
+        assert.strictEqual(actual, expected);
+    });
+    test("greet returns the correct greeting in Japanease", () => {
+        const expected = "Konnichiwa, Sahil!";
+        const actual = greetInJapanease("Sahil");
+        assert.strictEqual(actual, expected);
+    });
+});
